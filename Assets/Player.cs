ae8dfca8d2d29994603434388e027362c296cef8
Skybax12358 <1005809@student.sae.edu.au>
2016-12-13 18:34:06 +1000

    First Commit
    Boat is in the game and work for the start of the SHMUP

---
+++ Assets/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float Speed;
+
+	public GameObject Wall1;
+	public GameObject Wall2;
+	// Use this for initialization
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Movement();
+
+	}
+
+	void Movement()
+	{
+		transform.localPosition += transform.forward * Speed * Time.deltaTime;
+		transform.forward = Vector3.Normalize (new Vector3 (Input.GetAxis ("Horizontal"), 0f, Input.GetAxis ("Vertical")));
+	}
+
+
+	public void OnCollsionEnter (GameObject other)
+	{
+		if (other.gameObject.tag == "Wall1")
+			transform.position = new Vector3(Wall2.transform.position.x ,0f, Wall2.transform.position.z);
+
+		else if (other.gameObject.tag == "Wall2")
+			transform.position = new Vector3(Wall1.transform.position.x ,0f, Wall1.transform.position.z);
+	}	
+}
---
+++ Assets/PlayerLock.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerLock : MonoBehaviour {
+
+	public float Speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition += new Vector3(0f,0f,Speed * Time.deltaTime);
+	}
+}
